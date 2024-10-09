8aeaf0de558f13e570441016743ecb088ecf3296
Ben Healy <benjamin.p.healy@gmail.com>
2024-10-09 21:56:22 +0100

    Day4 part2

--- src/main/scala/Day04.scala
+++ src/main/scala/Day04.scala
@@ -1,11 +1,19 @@
 object Day04:
-    case class Room(encrypted: String, id: Int, checksum: String)
+    private case class Room(encrypted: String, id: Int, checksum: String)
 
     private def isRealRoom(room: Room): Boolean = {
         room.encrypted.groupBy(identity).view.mapValues(_.length).toSeq
         .sortBy((char: Char, count: Int) => (count * -1, char))
         .take(5).map(_.head).mkString
         .equals(room.checksum)
+    }
+
+    private def decryption(room: Room): String = {
+        room.encrypted.map(shift(_, room.id))
+    }
+
+    private def shift(char: Char, diff: Int): Char = {
+        ((((char - 96) + (diff % 26)) % 26) + 96).toChar
     }
 
     def part1(input: Seq[String]): Int = {
@@ -15,7 +23,15 @@ object Day04:
         }).filter(isRealRoom).foldLeft(0)((a,b) => a + b.id)
     }
 
+    def part2(input: Seq[String]): Int = {
+        input.map(l => {
+            val split = l.split("-")
+            Room(split.dropRight(1).mkString, split.last.filter(_.isDigit).toInt, split.last.substring(split.last.length - 6, split.last.length-1))
+        }).filter(decryption(_).equalsIgnoreCase("northpoleobjectstorage")).head.id
+    }
+
     def main(args: Array[String]): Unit = {
         val data = io.Source.fromResource("Day04.txt").getLines().toSeq
         println(part1(data))
+        println(part2(data))
     }
