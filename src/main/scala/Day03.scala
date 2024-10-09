68802acf6c194e12846224f006fd50b888b8e368
Ben Healy <benjamin.p.healy@gmail.com>
2024-10-09 16:34:57 +0100

    Day3part2

--- src/main/scala/Day03.scala
+++ src/main/scala/Day03.scala
@@ -1,4 +1,5 @@
 object Day03:
+    case class Bob(division: Int, modulos: Int)
 
     def isTriangle(input: String): Boolean = {
         val values: Seq[Int] = input.trim.split("\\s+").toSeq.map(_.toInt).sorted
@@ -9,8 +10,14 @@ object Day03:
         input.count(isTriangle)
     }
 
+    def part2(input: Seq[String]): Int = {
+        val flatInput: Seq[String] = input.flatMap(_.trim.split("\\s+").toSeq)
+        val verticalInput = flatInput.zipWithIndex.groupBy((s, i) => Bob(i/9, i%3)).values.map(t => t.map(m => m._1)).map(t => t.toSeq.mkString(" ")).toSeq
+        verticalInput.count(isTriangle)
+    }
 
     def main(args: Array[String]): Unit = {
         val data = io.Source.fromResource("Day03.txt").getLines().toSeq
         println(part1(data))
+        println(part2(data))
     }
--- src/test/scala/Day03Test.scala
+++ src/test/scala/Day03Test.scala
@@ -15,4 +15,13 @@ class Day03Test extends AnyFunSuite{
         assert(Day03.part1(Seq("16  10 25")) == 1)
     }
 
+    test("part2") {
+        val input = Seq("101 301 501",
+          "102 302 502",
+          "103 303 503",
+          "201 401 601",
+          "202 402 602", "203 403 603")
+        assert(Day03.part2(input) > 0)
+    }
+
 }
