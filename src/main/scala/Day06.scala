c00732e5adffd8862dd105bf9fddc9b0566b1ce5
Ben Healy <benjamin.p.healy@gmail.com>
2024-10-10 19:53:44 +0100

    Day6 part2

--- src/main/scala/Day06.scala
+++ src/main/scala/Day06.scala
@@ -2,7 +2,6 @@ import scala.collection.mutable
 import scala.collection.mutable.Map
 object Day06:
 
-
     def part1(input: Seq[String]): String = {
         val charsByColumn: mutable.Map[Int, Seq[Char]] = mutable.Map()
         Range(0,input.head.length).foreach(i => charsByColumn+=(i -> Seq.empty))
@@ -11,8 +10,17 @@ object Day06:
         charsByColumn.foldLeft(""){case (s, (i, sequence)) => s + sequence.groupBy(identity).maxBy(_._2.size)._1}
     }
 
+    def part2(input: Seq[String]): String = {
+      val charsByColumn: mutable.Map[Int, Seq[Char]] = mutable.Map()
+      Range(0, input.head.length).foreach(i => charsByColumn += (i -> Seq.empty))
+      input.foreach(s => s.zipWithIndex.foreach((c, i) => charsByColumn(i) = charsByColumn(i).appended(c)))
+
+      charsByColumn.foldLeft("") { case (s, (i, sequence)) => s + sequence.groupBy(identity).minBy(_._2.size)._1 }
+    }
+
 
     def main(args: Array[String]): Unit = {
         val data = io.Source.fromResource("Day06.txt").getLines().toSeq
         println(part1(data))
+        println(part2(data))
     }
--- src/test/scala/Day06Test.scala
+++ src/test/scala/Day06Test.scala
@@ -4,8 +4,12 @@ import org.scalatest.funsuite.AnyFunSuite
 class Day06Test extends AnyFunSuite{
     val sampleInput: Seq[String] = "eedadn\ndrvtee\neandsr\nraavrd\natevrs\ntsrnev\nsdttsa\nrasrtv\nnssdts\nntnada\nsvetve\ntesnvt\nvntsnd\nvrdear\ndvrsen\nenarar".split("\n")
 
-    test("abc is 18f47a30") {
+    test("sample is easter") {
         assert(Day06.part1(sampleInput) == "easter")
+    }
+
+    test("sample part 2 is advent") {
+        assert(Day06.part2(sampleInput) == "advent")
     }
 
 }
