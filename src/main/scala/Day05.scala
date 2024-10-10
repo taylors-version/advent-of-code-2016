76625f62f664f6f1ad8a5f92098f9b4882f93aea
Ben Healy <benjamin.p.healy@gmail.com>
2024-10-10 16:14:02 +0100

    Day5 Part2

--- src/main/scala/Day05.scala
+++ src/main/scala/Day05.scala
@@ -13,6 +13,9 @@ object Day05:
     @tailrec
     private def nextCode(index: Int, input: String): (Int, String) = {
         val hash = md5(input + index)
+        if (index == 3231929) then {
+          val ben = "boo"
+        }
         if hash.startsWith("00000") then (index, hash)
         else nextCode(index + 1 ,input)
     }
@@ -26,11 +29,27 @@ object Day05:
         }
     }
 
+    @tailrec
+    private def buildCode2(input: String, index: Int = 0, code: Seq[String] = Seq("", "", "", "", "", "", "", "")): Seq[String] = {
+        if !code.contains("") then code
+        else {
+            val hash = nextCode(index, input)
+            val pos = hash._2(5)
+            val newCode: Seq[String] = if pos.asDigit < 8 && code(pos.asDigit).equals("") then code.updated(pos.asDigit, hash._2(6).toString) else code
+            buildCode2(input, hash._1 + 1, newCode)
+        }
+    }
+
     def part1(input: String): String = {
         buildCode(input)
+    }
+
+    def part2(input: String): String = {
+        buildCode2(input).mkString
     }
 
     def main(args: Array[String]): Unit = {
         val data = io.Source.fromResource("Day05.txt").mkString
         println(part1(data))
+        println(part2(data))
     }
--- src/test/scala/Day05Test.scala
+++ src/test/scala/Day05Test.scala
@@ -3,7 +3,11 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class Day05Test extends AnyFunSuite{
 
-    test("abc first is 3231929") {
+    test("abc is 18f47a30") {
         assert(Day05.part1("abc") == "18f47a30")
+    }
+
+    test("part2 abc is 3231929") {
+        assert(Day05.part2("abc") == "05ace8e3")
     }
 }
