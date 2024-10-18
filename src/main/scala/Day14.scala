dd987f7143eab01767a05ec6498e502f4d06e65e
Ben Healy <benjamin.p.healy@gmail.com>
2024-10-18 22:43:52 +0100

    Day 14 Part 2

--- src/main/scala/Day14.scala
+++ src/main/scala/Day14.scala
@@ -4,6 +4,7 @@ import scala.collection.mutable
 
 object Day14:
     private val hashes = collection.mutable.HashMap[String, String]()
+    private val stretchedHashes = collection.mutable.HashMap[String, String]()
 
     private def md5(inputStr: String): String = {
         val md: MessageDigest = MessageDigest.getInstance("MD5")
@@ -14,31 +15,45 @@ object Day14:
         }
     }
 
+    @tailrec
+    private def stretchedmd5(inputStr: String, count: Int = 0): String = {
+        if count == 2017 then inputStr else stretchedmd5(md5(inputStr), count + 1)
+    }
+
     def getmd5(inputStr: String): String = {
         hashes.getOrElseUpdate(inputStr, md5(inputStr))
+    }
+
+    def getStretchedmd5(inputStr: String): String = {
+        stretchedHashes.getOrElseUpdate(inputStr, stretchedmd5(inputStr))
     }
 
     def containsTriple(hash: String): Option[String] = hash.sliding(3).find(s => s(0) == s(1) && s(0) == s(2))
     def containsFive(hash: String, char: Char): Boolean = hash.contains(char.toString * 5)
 
-    def isKey(prefix: String, suffix: Int): Boolean = {
-        val hash = getmd5(prefix + suffix.toString)
+    def isKey(prefix: String, suffix: Int, hashing: String => String): Boolean = {
+        val hash = hashing(prefix + suffix.toString)
         containsTriple(hash) match
             case None => false
-            case Some(s) => (for i <- 1 to 1000 yield containsFive(getmd5(prefix + (suffix + i).toString), s.head)).foldLeft(false)((a,b) => a || b)
+            case Some(s) => (for i <- 1 to 1000 yield containsFive(hashing(prefix + (suffix + i).toString), s.head)).foldLeft(false)((a,b) => a || b)
     }
 
     @tailrec
-    private def findKeys(prefix: String, suffix: Int, keyCount: Int = 0, counter: Int = 0): Int = {
+    private def findKeys(prefix: String, suffix: Int, keyCount: Int = 0, counter: Int = 0, hashing: String => String = getmd5): Int = {
         if keyCount == 64 then counter - 1 else
-            val newKeyCount = if isKey(prefix, suffix) then keyCount + 1 else keyCount
-            findKeys(prefix, suffix + 1, newKeyCount, counter + 1)
+            val newKeyCount = if isKey(prefix, suffix, hashing) then keyCount + 1 else keyCount
+            findKeys(prefix, suffix + 1, newKeyCount, counter + 1, hashing)
     }
 
     def part1(input: String): Int = {
         findKeys(input, 0)
     }
+
+    def part2(input: String): Int = {
+        findKeys(input, 0,  hashing = getStretchedmd5)
+    }
     
     def main(args: Array[String]): Unit = {
-        println(part1("ngcjuoqr"))
+        //println(part1("ngcjuoqr"))
+        println(part2("ngcjuoqr"))
     }
--- src/test/scala/Day14Test.scala
+++ src/test/scala/Day14Test.scala
@@ -1,25 +1,34 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class Day14Test extends AnyFunSuite{
-    
+
     test("abc18 is triple") {
         assert(Day14.containsTriple(Day14.getmd5("abc18")).nonEmpty)
     }
-    
+
     test("abc18 is not a key"){
-        assert(!Day14.isKey("abc", 18))
+        assert(!Day14.isKey("abc", 18, Day14.getmd5))
     }
-    
+
     test("abc816 is a f"){
         assert(Day14.containsFive(Day14.getmd5("abc816"), 'e'))
     }
-    
+
     test("abc39 is a key"){
-        assert(Day14.isKey("abc", 39))
+        assert(Day14.isKey("abc", 39, Day14.getmd5))
     }
-    
+
     test("Example = 22728") {
         assert(Day14.part1("abc") == 22728)
     }
+
+    test("stretched Hash of abc0 is a107ff634856bb300138cac6568c0f24"){
+        assert(Day14.getStretchedmd5("abc0").equals("a107ff634856bb300138cac6568c0f24"))
+    }
+
+    test("Example Stretched = 22551") {
+        assert(Day14.part2("abc") == 22551)
+    }
+    
 
 }
