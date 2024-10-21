635f53ba9b9ebac5db9876559fb8c202843cce50
Ben Healy <benjamin.p.healy@gmail.com>
2024-10-21 13:10:59 +0100

    Day 17 part 1

---
+++ src/main/scala/Day17.scala
@@ -0,0 +1,56 @@
+import java.security.MessageDigest
+import scala.collection.mutable
+
+object Day17:
+    private val openDoors = Seq('b', 'c', 'd', 'e', 'f')
+
+    private case class Coord(x: Int, y: Int, passcode: String, path: String):
+        def goal: Boolean = x == 3 && y == 3
+        def valid: Boolean =
+            x >= 0 && y >= 0 && x<=3 && y<=3 && validHash(md5(passcode + path.dropRight(1)), path.takeRight(1))
+
+        def next: Seq[Coord] = Seq(Coord(x - 1, y, passcode, path + "L"), Coord(x + 1, y, passcode, path + "R"), Coord(x, y - 1, passcode, path + "U"), Coord(x, y + 1, passcode, path + "D"))
+        private def validHash(hash: String, direction: String): Boolean =
+            val compareChar = direction match
+                case "U" => hash(0)
+                case "D" => hash(1)
+                case "L" => hash(2)
+                case _ => hash(3)
+            openDoors.contains(compareChar)
+
+    private def md5(inputStr: String): String = {
+        val md: MessageDigest = MessageDigest.getInstance("MD5")
+        md.digest(inputStr.getBytes()).map(0xFF & _).map {
+            "%02x".format(_)
+        }.foldLeft("") {
+            _ + _
+        }
+    }
+
+    private def dijkstra(start: Coord): Coord = {
+        val unvisitedStates = mutable.Queue[Coord](start)
+        val visitedStates = collection.mutable.Map(start -> 0)
+
+        while unvisitedStates.nonEmpty do
+            val currentState = unvisitedStates.dequeue()
+            if (currentState.goal) {
+                return currentState
+            }
+            val count = visitedStates(currentState) + 1
+            val next = currentState.next.filter(_.valid)
+            next.foreach { n =>
+                if!visitedStates.contains(n) || count < visitedStates(n) then
+                    visitedStates(n) = count
+                    unvisitedStates.enqueue(n)
+            }
+        start
+    }
+
+
+    def part1(input: String): String = {
+        dijkstra(Coord(0, 0, input, "")).path
+    }
+
+    def main(args: Array[String]): Unit = {
+        println(part1("awrkjxxr"))
+    }
