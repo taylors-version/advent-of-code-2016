488b1d77bf66bbe91e01f09239be1ee071a81692
Ben Healy <benjamin.p.healy@gmail.com>
2024-10-23 19:47:23 +0100

    Day 24 Part 2 Refactor to use Dijkstra

--- src/main/scala/Day24.scala
+++ src/main/scala/Day24.scala
@@ -1,4 +1,4 @@
-import scala.collection.immutable.Seq
+import scala.collection.immutable.{Seq, Set}
 import scala.collection.mutable
 
 object Day24:
@@ -6,18 +6,21 @@ object Day24:
         def next: Seq[Coord] = Seq(Coord(x+1, y), Coord(x-1, y), Coord(x, y+1), Coord(x, y-1))
     }
 
-    case class State(maze: Array[Array[Char]], position: Coord, charsCollected: Set[Char], steps: Int = 0) extends Ordered[State] {
+    case class State(maze: Array[Array[Char]], position: Coord, charsCollected: Set[Char], homePos: Coord, targetChars: Int, steps: Int) extends Ordered[State] {
         override def compare(that: State): Int = that.steps - this.steps
         override def equals(other: Any): Boolean = other match {
             case state: State =>
-                this.position == state.position && this.charsCollected == state.charsCollected
+                this.position == state.position && this.charsCollected == state.charsCollected && (this.maze sameElements state.maze)
             case _ => false
         }
-        override def hashCode(): Int = (position.hashCode + charsCollected.hashCode()).hashCode()
+        override def hashCode(): Int = (position.hashCode + charsCollected.hashCode() + maze.hashCode()).hashCode()
+        
+        def goal: Boolean = charsCollected.size == targetChars
+        def backHome: Boolean = position == homePos
 
         def next: Seq[State] = {
             position.next.filterNot(n => maze(n.x)(n.y) == '#').map(n =>
-                State(maze, n, charsCollected + maze(n.x)(n.y), steps+1)
+                State(maze, n, charsCollected + maze(n.x)(n.y), homePos, targetChars, steps+1)
             )
         }
     }
@@ -30,20 +33,24 @@ object Day24:
         maze
     }
 
-    private def bfs(startState: State, digitCount: Int): Int = {
-        val unvisitedStates = mutable.Queue[State](startState)
-        val visitedStates = collection.mutable.Map(startState -> 0)
+    private def dijkstra(start: State, finishAnywhere: Boolean = true): State = {
+        val unvisitedStates = mutable.PriorityQueue[State](start)
+        val visitedStates = mutable.HashSet[State]()
 
-        while unvisitedStates.nonEmpty do
+        while (unvisitedStates.nonEmpty) {
             val currentState = unvisitedStates.dequeue()
-            val count = visitedStates(currentState) + 1
-            val next = currentState.next
-            next.foreach { n =>
-                if !visitedStates.contains(n) || count < visitedStates(n) then
-                    visitedStates(n) = count
-                    unvisitedStates.enqueue(n)
-            }
-        visitedStates(visitedStates.keys.filter(_.charsCollected.size == digitCount).toSeq.minBy(_.steps))
+            visitedStates.add(currentState)
+            if (currentState.goal && (finishAnywhere || currentState.backHome))
+                return currentState
+            else
+                val next = currentState.next
+                next.foreach { n =>
+                    if !visitedStates.contains(n)  then
+                        visitedStates.add(n)
+                        unvisitedStates.enqueue(n)
+                }
+        }
+        start.copy(steps = Int.MaxValue)
     }
 
     def part1(input: Seq[String]): Int = {
@@ -54,10 +61,25 @@ object Day24:
             (y, j) <- x.zipWithIndex
             if maze(i)(j)=='0'
         } yield Coord(i,j)
-        val startState = State(maze, startPosition.head, Set('.', '0'))
-        bfs(startState, targetSize)
+        val startState = State(maze, startPosition.head, Set('.', '0'), startPosition.head, targetSize, 0)
+        dijkstra(startState).steps
     }
+
+    def part2(input: Seq[String]): Int = {
+        val maze = parseInput(input)
+        val targetSize = input.foldLeft(0)((a, b) => a + b.count(c => c.isDigit)) + 1 //Adding the '.' to this to make life easier
+        val startPosition = for {
+            (x, i) <- maze.zipWithIndex
+            (y, j) <- x.zipWithIndex
+            if maze(i)(j) == '0'
+        } yield Coord(i, j)
+        val startState = State(maze, startPosition.head, Set('.', '0'), startPosition.head, targetSize, 0)
+        dijkstra(startState, false).steps
+    }
+
+
     def main(args: Array[String]): Unit = {
         val data: Seq[String] = io.Source.fromResource("Day24.txt").getLines().toSeq
         println(part1(data))
+        println(part2(data))
     }
--- src/test/scala/Day24Test.scala
+++ src/test/scala/Day24Test.scala
@@ -5,7 +5,7 @@ import scala.collection.immutable.Seq
 class Day24Test extends AnyFunSuite{
     val input = "###########\n#0.1.....2#\n#.#######.#\n#4.......3#\n###########"
     
-    test("Example produces 3") {
+    test("Example produces 14") {
         val expected = 14
         val actual = Day24.part1(input.split("\\n"))
         assert(expected == actual)
